3143b13883a34126ec45c33d180e825ca3063825
Dyugurova Ekaterina <dugurowa.k@yandex.ru>
2022-10-16 20:26:39 +0400

    revision

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Post post = new Post();
-        FormDate formDate = new FormDate();
+        post.birthday = new FormDate();
 
         post.name = "Иван";
         post.passport = "4444 № 444444";
@@ -9,9 +9,9 @@ public class Main {
         post.phone = "7(999)-999-99-99";
         post.surname = "Иванов";
         post.subscription = true;
-        formDate.day = 13;
-        formDate.month = 6;
-        formDate.year = 1999;
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
 
 
     }
